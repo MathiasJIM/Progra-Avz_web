26271385f17dab95d71f7f48285b9553c8e1afba
ArthCA026 <r2chaves026@gmail.com>
2024-04-09 23:04:49 -0600

    Modelo de eventos en Web y API

---
+++ ProyectoFinalAtlas/Web/Abtracciones/Modelos/Evento.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstracciones.Models
+{
+    public class Evento
+    {
+        public Guid ID { get; set; }
+
+        [StringLength(255)]
+        public string Titulo { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public DateTime? FechaHoraInicio { get; set; }
+
+        public int? Duracion { get; set; }
+        public string Ubicacion { get; set; }
+
+        public string TipoEvento { get; set; }
+
+        public int? Concurrencia { get; set; }
+
+        public Guid? IDUsuarioCreacion { get; set; }
+
+        public DateTime? FechaHoraFinal { get; set; }
+    }
+}
