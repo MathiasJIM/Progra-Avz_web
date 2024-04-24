dced7eff85bfdd3cd9d824c7e59af52faa7b5096
RODRIGUEZ CASTILLO DAVID <drodriguez30398@ufide.ac.cr>
2024-04-24 17:02:57 -0600

    Nueva vistas de reglamentos

---
+++ ProyectoFinalAtlas/Web/Web/Pages/Reglamentos/Reglas.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using Web.Helpers;
+
+namespace Web.Pages.Reglamentos
+{
+    public class ReglasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
